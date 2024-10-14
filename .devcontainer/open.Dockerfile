998cfe910bd94370db94aec9d247bb13a8d6c38a
Ștefan Dan Ciocîrlan <stefan_dan@xn--ciocrlan-o2a.ro>
2024-10-14 12:26:54 +0300

    fix: update VPL simulation feat: add open source dockerfile

--- .devcontainer/open.Dockerfile
+++ .devcontainer/open.Dockerfile
@@ -1,17 +1,18 @@
 # syntax=docker/dockerfile:1
 FROM ubuntu:22.04
 
+LABEL maintainer="stefan_dan.ciocirlan@upb.ro"
+LABEL version="0.1"
+LABEL description="The open source toolchain docker image for the Computer Architecture course"
+
 # for apt-get
 # https://serverfault.com/questions/949991/how-to-install-tzdata-on-a-ubuntu-docker-image
 ARG DEBIAN_FRONTEND=noninteractive
 ENV TZ=Europe/Bucharest
 
-RUN apt-get update -y && \
+RUN apt-get update && \
     apt-get upgrade -y && \
-    apt-get install -y nano \
-    git unzip wget gedit
-
-
+    apt-get install -y nano git unzip wget gedit make gcc g++
 # install gtkwave through apt-get
 # https://github.com/gtkwave/gtkwave
 RUN apt-get install -y gtkwave
@@ -19,3 +20,8 @@ RUN apt-get install -y gtkwave
 # install icaurs
 # https://github.com/steveicarus/iverilog
 RUN apt-get install -y iverilog
+
+# install yosys
+RUN apt-get install -y yosys
+# install verilator
+RUN apt-get install -y verilator
--- chapters/verilog/basic/assigments/mux/test_mux.v
+++ chapters/verilog/basic/assigments/mux/test_mux.v
@@ -1,51 +1,16 @@
 `timescale 1ns / 1ps
-module test_mux #(
-    parameter p_sel_width = 2
-);
+module test_mux;
     //Inputs
-    reg [(p_sel_width-1):0] l_r_sel;
-    reg [(2**p_sel_width)-1:0] l_r_in;
 
     //Outputs
-    wire l_w_out;
     
     //local variables for loop
     integer i,j;
 
     //Module initialization
-    mux #(
-        .sel_width(p_sel_width)
-    ) uut (
-        .o_w_out(l_w_out),
-        .i_w_in(l_r_in),
-        .i_w_sel(l_r_sel)
-    );
-    
+
     //Simulation tests
     initial begin
-        //wave files
-        $dumpfile("test.vcd");
-        // dumpp all variables
-        $dumpvars;
-        // monitor varibles changes in values
-        $monitor(
-            "Time = %0t, ", $time,
-            "l_w_out = %0h, ", l_w_out,
-            "l_r_sel = %0h, ", l_r_sel,
-            "l_r_in = %0h", l_r_in
-        );
-
-        for (i = 0; i < (2**p_sel_width); i = i + 1) begin
-            l_r_in = 1 << i;
-            for (j = 0; j < (2**p_sel_width); j = j + 1) begin
-                l_r_sel = j;
-                #5;
-                if (l_w_out !== l_r_in[j]) begin
-                    $display("Error: l_w_out = %0h, l_r_in[%0d] = %0h", l_w_out, j, l_r_in[j]);
-                end
-                #5;
-            end
-        end
         //finish the simulation
         $finish;
     end
